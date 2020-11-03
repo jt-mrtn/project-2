69c2ab59a608df0b888cb6913439c46cceb9fb95
JT Martin <72337415+jt-mrtn@users.noreply.github.com>
2020-11-03 17:23:26 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
