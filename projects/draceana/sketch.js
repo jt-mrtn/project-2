a278ce7166821a1e62da5d5c910a54500153fc57
JT Martin <72337415+jt-mrtn@users.noreply.github.com>
2021-03-12 17:56:19 -0600

    Revamp folder structure

---
+++ projects/draceana/sketch.js
@@ -0,0 +1,178 @@
+// Title: Dracaena
+// Author: J.T. Martin
+
+// store pixel size of images
+var s = 237;
+
+//
+var draw_position_x = 0;
+var yn;
+var hn;
+var hx;
+var sn;
+var center;
+var font;
+var img;
+var img1;
+var img2;
+var img3;
+var hue;
+
+var click = 1;
+
+function preload() {
+  font = loadFont("Robus-BWqOd.otf"); // (via license: Trans ID 6L014138AG223504L Creative Fabrica BV)
+  img1 = loadImage("images/plant1.jpg");
+  img2 = loadImage("images/plant2.jpg");
+  img3 = loadImage("images/plant3.jpg");
+  img4 = loadImage("images/plant4.jpg");
+}
+
+function mousePressed() {
+  click++;
+  if (click > 4) click = 1;
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  rectMode(CENTER);
+  background(0);
+  textFont(font);
+  center = width / 3;
+}
+
+function draw() {
+  if (click == 1) {
+    img = img1;
+  } else if (click == 2) {
+    img = img2;
+  } else if (click == 3) {
+    img = img3;
+  } else if (click == 4) {
+    img = img4;
+  }
+
+  img.loadPixels();
+  var mx = constrain(mouseX, center - s / 2, center + s / 2);
+  var x = map(mx, center - s / 2, center + s / 2, s, 0);
+  x = floor(x);
+
+  // graphic items in the background · · · · · · ·
+  for (var y = 0; y < height; y++) {
+    var c = img.get(x, y);
+    set(draw_position_x, y, c);
+    set(draw_position_x + s + s, y + s, c);
+    set(draw_position_x + 4 * s, y + 2 * s, c);
+    set(draw_position_x - 6, y + 3 * s, c);
+  }
+  updatePixels();
+  strokeWeight(1);
+  noFill();
+
+  click == 4 ? stroke(255) : stroke(0, 90);
+  for (let i = 0; i < height; i += 3) {
+    line(0, i, width, i);
+  }
+
+  image(img, mx, height - s / 2);
+
+  image(img, mx + 2.5 * s, height - s);
+
+  image(img, mx - s * 1.5, height - 3 * s);
+
+  if (click == 1) {
+    hue = "rgba(35,178,254,255)"; // light blue
+    yn = 70;
+    hn = height;
+    hx = hue;
+    sn = s;
+  } else if (click == 2) {
+    hue = "rgba(54, 99, 217, 255)"; // dark blue
+    yn = -140;
+    hn = height * 0.9;
+    hx = hue;
+    sn = 200;
+  } else if (click == 3) {
+    hue = "rgba(220, 31, 61, 255)"; // coral
+    yn = 103;
+    hn = height * 1.5;
+    hx = hue;
+    sn = 100;
+  } else if (click == 4) {
+    hue = "rgba(170, 220, 47, 255)"; // lime
+    yn = 7;
+    hn = height * 1.25;
+    hx = 0;
+    sn = 250;
+  }
+
+  stroke(hue);
+  //line(width / 3, height - s, width / 3, height);
+
+  rect(mx - s / 2, height - s, 237, 237);
+  rect(mx + 2 * s, height - s * 1.5, 237, 237);
+  rect(mx - 2 * s, height - 3.5 * s, 237, 237);
+
+  fill(0);
+  strokeWeight(2);
+  textSize(320);
+  var offset = 0;
+  for (let j = yn; j < height * 1.5; j += 169) {
+    for (let i = -480; i < width * 2.5; i += 863) {
+      text("Dracaena", i - offset, j);
+    }
+    offset += 480;
+  }
+
+  // build frame
+  click == 4 ? fill(255) : fill(0);
+  rectMode(CORNER);
+  noStroke();
+  // left
+  rect(0, 0, 150, height);
+  // right
+  rect(width - 150, 0, 150, height);
+  // top
+  rect(0, 0, width, 150);
+  // bottom
+  rect(0, height - 150, width, 150);
+
+  // graphic items in the foreground · · · · · · ·
+
+  // return stroke
+  if (click == 1) {
+    hue = "rgba(35,178,254,255)"; // light blue
+  } else if (click == 2) {
+    hue = "rgba(206, 118, 80, 255)"; // orange
+  } else if (click == 3) {
+    hue = "rgba(250, 134, 181, 255)"; // pink
+  } else if (click == 4) {
+    hue = "rgba(255, 255, 255, 255)"; // white
+  }
+
+  fill(hx);
+  stroke(hx);
+  strokeWeight(1);
+  ellipse(mx, height - 75, 18, 18);
+  line(width / 3, height - 150, width / 3, height - 66);
+
+  stroke(hue);
+  strokeWeight(2);
+
+  image(img, mx + 3 * sn, hn * 0.25);
+
+  image(img, mx - sn / 2, hn / 2 - 4);
+
+  noFill();
+  rectMode(CENTER);
+  rect(mx + 3 * sn, hn * 0.25, 237, 237);
+  rect(mx - sn / 2, hn / 2 - 4, 237, 237);
+  rectMode(CORNER);
+  // loop back around
+  draw_position_x++;
+
+  if (draw_position_x >= width) {
+    draw_position_x = 0;
+  }
+}
---
+++ projects/eyes/sketch.js
@@ -0,0 +1,570 @@
+// Kaleidoscope Eyes
+// Author: J.T. Martin
+
+// 1. Left, right and middle mouse buttons cause eyes to change.
+// 2. Press Esc key after pressing right button.
+// 3. There are 4 different sizes. Refresh page to return to the smallest.
+
+var x = 0;
+var y = 0;
+var px = 0;
+var py = 0;
+var mult = 1.6;
+var easing = 0.06;
+var radius = 8;
+var pupils = 4;
+var spacing = 40;
+var boom = 60;
+var count = 1;
+var click = 1;
+// default yellow - opening mood
+var r = 254;
+var g = 213;
+var b = 38;
+var e = 255;
+var h = 2;
+var z = 0;
+var angle = 0.0;
+var speed = 0.09;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  textFont("Industrial");
+}
+
+// advancement of different moods
+function mousePressed() {
+  click++;
+  if (click > 2) click = 1;
+}
+
+function draw() {
+  textSize(30);
+  textStyle(NORMAL);
+  fill(r, g, b, 6);
+
+  if (click == 1) {
+    if (mouseButton != RIGHT && mouseButton != LEFT && mouseButton != CENTER) {
+      text(
+        "Clicked on the link and still in the dark? You poor thing. If you caress and by caress",
+        width / 3.5,
+        height - 154
+      );
+      text(
+        "I mean press your mouse buttons individually, you'll encounter different moods. Use all",
+        width / 3.5,
+        height - 122
+      );
+      text(
+        "of them, even your center one (scroll wheel). Sway your little plasticine gnawer to and",
+        width / 3.5,
+        height - 90
+      );
+      text(
+        "fro. What happens? Go fast or slow. Hold the button of your choice down. What happens then?",
+        width / 3.5,
+        height - 58
+      );
+      text(
+        "For the right mouse button, press the Esc key to get rid of the pop-up menu. Have fun!",
+        width / 3.5,
+        height - 26
+      );
+    }
+  }
+  noFill();
+
+  var targetX = mouseX;
+  x += (targetX - x) * easing;
+  var targetY = mouseY;
+  y += (targetY - y) * easing;
+  var weight = dist(x, y, px, py) * 3;
+  w = map(x, 0, width, 0, 255);
+  z = map(y, 0, height, 0, 255);
+
+  // set up psychedelic backgrounds
+  if (mouseButton == LEFT && click == 2) {
+    // set up lucy in the sky with diamonds
+    var rx = map(mouseX, 0, width, 238, 254);
+    var gx = map(mouseX, 0, width, 0, 213);
+    var bx = 0;
+    mouseX < width / 2 ? (bx = 130) : (bx = 38);
+    background(rx, gx + z / 5, bx, 7);
+    strokeWeight(10);
+    push();
+    translate(width / 2, height / 2);
+    rotate(angle);
+    for (var i = -3 * width; i < 3 * width; i += 37) {
+      rx = 76;
+      gx = 233;
+      bx = 254;
+      stroke(rx, gx, bx);
+      line(i, -3 * height, i, 3 * height);
+    }
+
+    for (var i = -3 * height; i < 3 * height; i += 37) {
+      rx = 76;
+      gx = 233;
+      bx = 254;
+      stroke(rx, gx, bx);
+      line(-3 * width, i, 3 * width, i);
+    }
+
+    // rotating diamonds
+    pop();
+  } else if (mouseButton == CENTER) {
+    background(0, 0, 196 - w * 0.9, 7);
+    push();
+    translate(width * 1.75, height * 0.75);
+    rotate(angle * 0.9);
+    strokeWeight(1);
+    for (let i = -150; i < 2 * width + 150; i += 100 * 1.6) {
+      for (let j = -150; j < 2 * height + 150; j += 100 * 1.6) {
+        let r = random(60);
+        stroke(255);
+        point(width - i * r, 91 * r + j);
+      }
+    }
+    pop();
+  } else if (mouseButton == RIGHT) {
+    var rxx = map(mouseX, 0, width, 14, 201);
+    var gxx = map(mouseX, 0, width, 44, 255);
+    var bxx = map(mouseX, 0, width, 21, 255);
+    background(rxx, gxx + w * 10, bxx + z * 10, 2);
+    strokeWeight(1);
+    stroke(0, 2);
+    for (let i = 5; i < height; i += 5) {
+      line(0, i, width, i);
+    }
+  } else {
+    background(0, 5); // do I need to specify for click == 1 ?
+  }
+
+  // set up eye colors
+  if (mouseIsPressed) {
+    if ((mouseButton == LEFT) & (click == 2)) {
+      // blue
+      r = 55;
+      g = 40;
+      b = 252;
+      e = 240;
+      mult = 1.6;
+      boom = 20;
+      radius = 16;
+      pupils = radius / 2;
+      spacing = 107;
+      easing = 0.07;
+    } else if (mouseButton == RIGHT) {
+      // aqua
+      r = 86;
+      g = 206;
+      b = 175;
+      e = 250;
+      mult = 2.7;
+      boom = 40;
+      radius = 36;
+      pupils = radius / 2;
+      spacing = 283;
+      easing = 0.0108;
+    } else if (mouseButton == CENTER) {
+      // sky blue
+      r = 76;
+      g = 233;
+      b = 254;
+      e = 255;
+      mult = 1;
+      boom = 80;
+      radius = 118;
+      pupils = radius / 2;
+      spacing = 460;
+      easing = 0.09;
+    }
+  }
+
+  strokeWeight(weight * boom);
+  stroke(r, g, b - weight, 50 - 3 * weight);
+  line(x, y, px, py);
+  line(x + spacing, y, px + spacing, py);
+  px = x;
+  py = y;
+
+  // eyeline
+  if ((mouseButton == LEFT) & (click == 2)) {
+    noStroke();
+  } else {
+    strokeWeight(5);
+    noFill();
+    // left
+    arc(x, y, radius * h * mult, radius * h + weight, 0, 360);
+    // right
+    arc(x + spacing, y, radius * h * mult, radius * h + weight, 0, 360);
+  }
+
+  // left & center Button Brow
+  if (mouseButton == CENTER || (mouseButton == LEFT && click == 2)) {
+    // big eye line
+    if (mouseButton == LEFT && click == 2) {
+      noStroke();
+    } else {
+      strokeWeight(1);
+      stroke(r, g, b - weight, 50 - 3 * weight);
+      arc(x, y, radius * h * mult * 1.6, radius * h * 1.6, 0, 360);
+      arc(x + spacing, y, radius * h * mult * 1.6, radius * h * 1.6, 0, 360);
+    }
+
+    //left big arc
+    noStroke();
+    if (mouseIsPressed) {
+      fill(z, g - z, b + z);
+    } else {
+      fill(z, g - z, b + z, 50 - 3 * weight);
+    }
+    arc(
+      x,
+      y,
+      radius * h * mult * 1.6,
+      radius * h * 1.6,
+      270 + 5 * weight,
+      10 - 5 * weight
+    );
+    arc(
+      x,
+      y,
+      radius * h * mult * 1.6,
+      radius * h * 1.6,
+      30 + 5 * weight,
+      130 - 5 * weight
+    );
+    arc(
+      x,
+      y,
+      radius * h * mult * 1.6,
+      radius * h * 1.6,
+      150 + 5 * weight,
+      250 - 5 * weight
+    );
+    // right big arc
+    arc(
+      x + spacing,
+      y,
+      radius * h * mult * 1.6,
+      radius * h * 1.6,
+      180 + 5 * weight,
+      280 - 5 * weight
+    );
+    arc(
+      x + spacing,
+      y,
+      radius * h * mult * 1.6,
+      radius * h * 1.6,
+      300 + 5 * weight,
+      40 - 5 * weight
+    );
+    arc(
+      x + spacing,
+      y,
+      radius * h * mult * 1.6,
+      radius * h * 1.6,
+      60 + 5 * weight,
+      160 - 5 * weight
+    );
+
+    // center eyeline
+    strokeWeight(1);
+    if (mouseButton == LEFT && click == 2) {
+      noStroke();
+    } else if (mouseIsPressed) {
+      stroke(r, g, b - weight);
+    } else {
+      stroke(r, g, b - weight, 50 - 3 * weight);
+      noFill();
+      arc(x, y, radius * h * mult * 1.3, radius * h * 1.3, 0, 360);
+      arc(x + spacing, y, radius * h * mult * 1.3, radius * h * 1.3, 0, 360);
+    }
+    // left center arc
+    if (mouseIsPressed) {
+      fill(230, 250 - 3 * weight);
+    } else {
+      fill(230, 50 - 3 * weight);
+    }
+    noStroke();
+    arc(
+      x,
+      y,
+      radius * h * mult * 1.3,
+      radius * h * 1.3,
+      0 + 5 * weight,
+      100 - 5 * weight
+    );
+    arc(
+      x,
+      y,
+      radius * h * mult * 1.3,
+      radius * h * 1.3,
+      120 + 5 * weight,
+      220 - 5 * weight
+    );
+    arc(
+      x,
+      y,
+      radius * h * mult * 1.3,
+      radius * h * 1.3,
+      250 + 5 * weight,
+      350 - 5 * weight
+    );
+
+    // right center arc
+    arc(
+      x + spacing,
+      y,
+      radius * h * mult * 1.3,
+      radius * h * 1.3,
+      201 + 5 * weight,
+      301 - 5 * weight
+    );
+    arc(
+      x + spacing,
+      y,
+      radius * h * mult * 1.3,
+      radius * h * 1.3,
+      321 + 5 * weight,
+      61 - 5 * weight
+    );
+    arc(
+      x + spacing,
+      y,
+      radius * h * mult * 1.3,
+      radius * h * 1.3,
+      81 + 5 * weight,
+      181 - 5 * weight
+    );
+  }
+
+  // "whites" of the eyes
+  noStroke();
+  strokeWeight(1);
+  if (mouseIsPressed) {
+    fill(255, 250 - 3 * weight);
+  } else {
+    fill(255, 80 - 3 * weight);
+  }
+  // left;
+  arc(x, y, radius * h * mult, radius * h, 100 + 5 * weight, 190 - 5 * weight);
+
+  arc(x, y, radius * h * mult, radius * h, 210 + 5 * weight, 310 - 5 * weight);
+
+  arc(x, y, radius * h * mult, radius * h, 330 + 5 * weight, 80 - 5 * weight);
+
+  // right
+  arc(
+    x + spacing,
+    y,
+    radius * h * mult,
+    radius * h,
+    150 + 5 * weight,
+    240 - 5 * weight
+  );
+
+  arc(
+    x + spacing,
+    y,
+    radius * h * mult,
+    radius * h,
+    260 + 5 * weight,
+    360 - 5 * weight
+  );
+
+  arc(
+    x + spacing,
+    y,
+    radius * h * mult,
+    radius * h,
+    20 + 5 * weight,
+    130 - 5 * weight
+  );
+
+  noStroke();
+  // iris;
+  fill(r + z, g - z, b - z);
+  ellipse(x, y, radius - weight, radius - weight);
+  if (mouseButton == CENTER) {
+    fill(r + z, g - z, b + z);
+  }
+  ellipse(x + spacing, y, radius - weight, radius - weight);
+
+  // pupils
+  if (mouseY > height / 2) {
+    fill(0 + w, 200 - z, 170 - z);
+  } else {
+    fill(0 + z, 42, 82 + w);
+  }
+
+  ellipse(x, y, pupils - weight, pupils - weight);
+  ellipse(x + spacing, y, pupils - weight, pupils - weight);
+  // eyebrows no mouse
+  //if (mouseButton != LEFT && mouseButton != RIGHT && mouseButton != CENTER) {
+  if (mouseButton == LEFT && click == 1) {
+    noFill();
+    stroke(234 - z, 107 - w, 20 - z, 40);
+    strokeWeight(2);
+    // left
+    arc(
+      x,
+      y,
+      radius * mult * 4 + weight * 30,
+      radius * 4 + weight * 70,
+      180,
+      275
+    );
+    // right
+    arc(
+      x + spacing,
+      y,
+      radius * mult * 4 + weight * 30,
+      radius * 4 + weight * 70,
+      270,
+      350
+    );
+    // lines under eyes
+    stroke(234 - z, 107 - w, 20 - z, 5);
+    arc(x - 7, y, radius * mult * 4, radius * 4, 18, 88);
+    arc(x + spacing + 7, y, radius * mult * 4, radius * 4, 100, 160);
+
+    // some shade over little yellow eyes
+    noStroke();
+    fill(r, g - w, b - z, 1);
+    arc(x - 7, y, radius * mult * 4, radius * 4, 0, 360);
+    arc(x + spacing + 7, y, radius * mult * 4, radius * 4, 0, 360);
+  } else if (mouseButton == RIGHT) {
+    // eyebrows - right
+    noFill();
+    stroke(234 - z, 107 - w, 20 - z);
+    strokeWeight(3);
+    // left
+    arc(x, y, radius * mult * 3, radius * 3, 200, 335);
+
+    // right
+    arc(x + spacing, y, radius * mult * 3, radius * 3, 210, 340);
+
+    strokeWeight(1);
+    stroke(234 - z, 107 - w, 20 - z, 30);
+    arc(x, y + 90, radius * mult * 2, radius * 2, 280, 325);
+    arc(x + spacing, y + 90, radius * mult * 2, radius * 2, 230, 265);
+
+    // eyelashes
+    // left
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 193, 194);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 196, 197);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 199, 200);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 202, 203);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 205, 206);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 208, 209);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 211, 212);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 214, 215);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 217, 218);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 220, 221);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 223, 224);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 226, 227);
+
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 229, 230);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 232, 233);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 235, 236);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 238, 239);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 241, 242);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 244, 245);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 247, 248);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 250, 251);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 253, 254);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 256, 257);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 259, 260);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 262, 263);
+
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 265, 266);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 268, 269);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 271, 272);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 274, 275);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 277, 278);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 280, 281);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 283, 284);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 286, 287);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 289, 290);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 292, 293);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 295, 296);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 298, 299);
+
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 301, 302);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 304, 305);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 307, 308);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 310, 311);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 313, 314);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 316, 317);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 319, 320);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 322, 323);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 325, 326);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 328, 329);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 331, 332);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 334, 335);
+
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 337, 338);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 340, 341);
+    arc(x, y, radius * mult * 2.5, radius * 2.2, 343, 344);
+
+    //right eyelashes
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 199, 200);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 202, 203);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 205, 206);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 208, 209);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 211, 212);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 214, 215);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 217, 218);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 220, 221);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 223, 224);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 226, 227);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 229, 230);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 232, 233);
+
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 235, 236);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 238, 239);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 241, 242);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 244, 245);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 247, 248);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 250, 251);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 253, 254);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 256, 257);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 259, 260);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 262, 263);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 265, 266);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 268, 269);
+
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 271, 272);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 274, 275);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 277, 278);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 280, 281);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 283, 284);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 286, 287);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 289, 290);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 292, 293);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 295, 296);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 298, 299);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 301, 302);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 304, 305);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 307, 308);
+
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 310, 311);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 313, 314);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 316, 317);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 319, 320);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 322, 323);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 325, 326);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 328, 329);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 331, 332);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 334, 335);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 337, 338);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 340, 341);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 343, 344);
+    arc(x + spacing, y, radius * mult * 2.5, radius * 2.2, 346, 347);
+  }
+  angle += 1 * speed;
+  count++;
+}
